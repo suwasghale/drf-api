b50fe49f0e0c02e776cbb630000017bcc3118e3b
SuwasG <suwasghale693@gmail.com>
2025-08-23 20:54:08 +0545

    user, register and me endpoint views with viewsets

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer, RegisterSerializer
 
 # Create your views here.
+# List and Detail Read Only
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+# register endpoint
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+# "Me" endpoint
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
