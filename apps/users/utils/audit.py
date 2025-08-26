8e57777f983d63d8baa6fd5176e2542e471c3212
SuwasG <suwasghale693@gmail.com>
2025-08-26 10:07:14 +0545

    user log activity added

---
+++ apps/users/utils/audit.py
@@ -0,0 +1,17 @@
+from django.utils import timezone
+# from apps.users.models import UserActivityLog
+from ..models import UserActivityLog
+
+def log_user_activity(user, action):
+    """
+    Logs user actions for auditing.
+    :param user -> User Instance,
+    :param action -> Action Description (String) 
+    """
+    if user and action:
+        UserActivityLog.objects.create(
+            user = user,
+            action = action,
+            timestamp = timezone.now()
+        )
+
