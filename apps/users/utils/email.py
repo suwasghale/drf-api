b5f93f8339b363fe317bcc046adc8eec6fe48060
SuwasG <suwasghale693@gmail.com>
2025-08-26 07:02:29 +0545

    password reset email utils

--- apps/users/utils/email.py
+++ apps/users/utils/email.py
@@ -17,3 +17,16 @@ def send_verification_email(to_email, username, verification_url):
     email = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [to_email])
     email.attach_alternative(html_content, "text/html")
     email.send()
+
+
+def send_password_reset_email(to_email, username, reset_url):
+    subject = "Reset your password"
+    text_content = f"Hi {username}, reset your password here: {reset_url}"
+    html_content = f"""
+    <p>Hi {username},</p>
+    <p>Click the button below to reset your password:</p>
+    <p><a href="{reset_url}">Reset Password</a></p>
+    """
+    email = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [to_email])
+    email.attach_alternative(html_content, "text/html")
+    email.send()
