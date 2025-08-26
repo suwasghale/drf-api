accb6fc82ae567a5a57f2a3781c8dbadaacc7c7a
SuwasG <suwasghale693@gmail.com>
2025-08-26 09:47:09 +0545

    user activity log model created

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
+# Create your models here.
 class User(AbstractUser):
     """
     Custom User Model extending Django's AbstractUser. 
@@ -12,3 +12,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class UserActivityLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activity_logs')
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.action} at {self.timestamp}"
