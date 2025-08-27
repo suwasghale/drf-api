593312eb8881d9a5cf84b2f81db44d67eb0a4081
SuwasG <suwasghale693@gmail.com>
2025-08-27 22:09:21 +0545

    custom pagination updated

--- apps/product/pagination.py
+++ apps/product/pagination.py
@@ -3,12 +3,13 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-    page_size = 4
+    page_size = 1
     page_size_query_param = 'page_size'
     max_page_size = 50
 
     def get_paginated_response(self, data):
-        total_pages = ceil(self.page.paginator.count / self.page_size)
+        page_size = self.get_page_size(self.request) or self.page_size
+        total_pages = ceil(self.page.paginator.count / page_size)
         return Response({
             'links': {
                 'next': self.get_next_link(),
--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 4,
+    'PAGE_SIZE': 1,
 
 }
 REST_FRAMEWORK.update({
