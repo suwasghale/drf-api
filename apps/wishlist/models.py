96083d66860c49c3ce809ce072cd03e4c5ce5859
SuwasG <suwasghale693@gmail.com>
2025-09-01 06:53:01 +0545

    models created for wishlist

--- apps/wishlist/models.py
+++ apps/wishlist/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from apps.product.models import Product
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("wishlist", "product")
