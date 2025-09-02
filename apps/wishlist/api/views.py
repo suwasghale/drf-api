7f183491365fb6231d2953e52614d6e0b777b810
SuwasG <suwasghale693@gmail.com>
2025-09-02 20:45:58 +0545

    add product on wishlist views created

---
+++ apps/wishlist/api/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from apps.wishlist.models import Wishlist, WishlistItem
+from .serializers import WishlistSerializer
+from apps.product.models import Product
+
+class WishlistViewSet(viewsets.ModelViewSet):
+    serializer_class = WishlistSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Return only wishlists for the logged-in user
+        return Wishlist.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def add_product(self, request, pk=None):
+        wishlist = self.get_object()
+        product_id = request.data.get('product_id')
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({"detail": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        item, created = WishlistItem.objects.get_or_create(wishlist=wishlist, product=product)
+        if not created:
+            return Response({"detail": "Product already in wishlist"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"detail": "Product added to wishlist"}, status=status.HTTP_201_CREATED)
+
+
