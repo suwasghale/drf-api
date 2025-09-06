12126a8a7fcb2e051ebf193e4e36013e1d06677d
SuwasG <suwasghale693@gmail.com>
2025-09-06 17:49:43 +0545

    updated services with proper explanations

--- apps/cart/services.py
+++ apps/cart/services.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
 from .models import Cart, CartItem
 from apps.product.models import Product 
+from django.db import transaction
 
 def get_or_create_cart(user):
     cart, _ = Cart.objects.get_or_create(user = user)
     return cart
 
-def add_to_cart(user, product_id, quantity):
+@transaction.atomic # NOTE: 
+def add_to_cart(user, product_id, quantity=1):
     cart = get_or_create_cart(user)
     product = get_object_or_404(Product, id=product_id)
     item, created_at = CartItem.objects.get_or_create(cart=cart, product=product)
