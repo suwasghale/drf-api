b7908ba4f14de950a9ffc41dab305bf3976ff615
SuwasG <suwasghale693@gmail.com>
2025-09-07 18:00:05 +0545

    order model created

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
+from apps.product.models import Product
 # Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("shipped", "Shipped"),
+        ("completed", "Completed"),
+        ("cancelled", "Cancelled")
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user.username}"
