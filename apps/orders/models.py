5ea8c0a55c52329b504015c8bc09820c9b4548c6
SuwasG <suwasghale693@gmail.com>
2025-09-07 18:03:09 +0545

    order item model created

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -17,3 +17,15 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # store product price at time of purchase
+
+    def get_total(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} (Order #{self.order.id})"
