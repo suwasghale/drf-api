d541e52be13582598d72344c71472012c59646c5
SuwasG <suwasghale693@gmail.com>
2025-09-11 12:32:40 +0545

    payment create serializer

--- apps/payments/api/serializers.py
+++ apps/payments/api/serializers.py
@@ -28,7 +28,11 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
     """
     Detail serializer with nested order information
     """
-    order = serializers.SerializerMethodField()
+    order = serializers.SerializerMethodField() 
+    """
+    This defines a field named order, but its value is not directly taken from a model field.
+    SerializerMethodField indicates that the field's value will be populated by calling a method on the serializer itself.
+    """
     remaining_balance = serializers.SerializerMethodField()
     is_fully_paid = serializers.SerializerMethodField()
 
@@ -46,7 +50,11 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
             "remaining_balance",
             "is_fully_paid",
         ]
-
+    """
+    The given is the method for the order = serializers.SerializerMethodField() field. DRF automatically looks for a method named get_<field_name>.
+    It takes self and the object being serialized (obj, which is a Payment instance).
+    It returns a dictionary containing specific details from the related order object, creating a nested representation of the order information.
+    """
     def get_order(self, obj):
         order = obj.order 
         return {
@@ -63,3 +71,35 @@ class PaymentDetailSerializer(serializers.ModelSerializer):
     def get_is_fully_paid(self, obj):
         return obj.order.is_fully_paid
 
+
+class PaymentCreateSerializer(serializers.ModelSerializer):
+    """
+     Create new payments with validations
+    """
+    class Meta:
+        model = Payment
+        fields = ["order", "amount", "gateway", "gateway_ref"]
+
+    def validate(self, attrs):
+        order = attrs["order"]
+        amount = attrs["amount"]
+
+        if amount <= 0:
+            raise serializers.ValidationError("Amount must be greater than 0.")
+
+        if amount > order.remaining_balance:
+            raise serializers.ValidationError(
+                f"Amount {amount} exceeds remaining balance {order.remaining_balance}."
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        payment = Payment.objects.create(**validated_data)
+
+        # Business rule: COD → mark completed immediately
+        if payment.gateway == "cod":
+            payment.status = "completed"
+            payment.save(update_fields=["status"])
+
+        return payment
