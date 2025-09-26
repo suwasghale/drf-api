88e438c2806937c46925dfa4a7451346df465452
SuwasG <suwasghale693@gmail.com>
2025-09-26 21:56:47 +0545

    updated admin register for model of users and addresses

--- apps/addresses/admin.py
+++ apps/addresses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from apps.addresses.models import Country, State, Address
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(Address)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import User, UserActivityLog, PasswordHistory, Country, State, Address
+from .models import User, UserActivityLog, PasswordHistory
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserActivityLog)
 admin.site.register(PasswordHistory)
-admin.site.register(Country)
-admin.site.register(State)
-admin.site.register(Address)
