267160bc973c32cb1f60af702bbf1896ab8190c3
SuwasG <suwasghale693@gmail.com>
2025-09-28 21:15:57 +0545

    user activity log inline created

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -9,6 +9,29 @@ class PasswordHistoryInline(admin.TabularInline):
     can_delete = False
     ordering = ("-timestamp",)
     verbose_name_plural = "Password Change History"\
+
+# ✅ Inline: Show latest few user activities inside User detail
+class UserActivityLogInline(admin.TabularInline):
+    model = UserActivityLog
+    extra = 0
+    readonly_fields = (
+        "action",
+        "timestamp",
+        "ip_address",
+        "user_agent",
+        "location",
+        "outcome",
+    )
+    can_delete = False
+    ordering = ("-timestamp",)
+    show_change_link = False
+    verbose_name_plural = "Recent Activity Logs"
+
+    def get_queryset(self, request):
+        """Show only last 10 logs for performance."""
+        qs = super().get_queryset(request)
+        return qs.order_by("-timestamp")[:10]
+    
     
 admin.site.register(User)
 admin.site.register(UserActivityLog)
