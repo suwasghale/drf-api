18ccf86e43240634b2514ddef82657d058401dd5
SuwasG <suwasghale693@gmail.com>
2025-09-28 21:15:22 +0545

    password history inline added

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from apps.users.models import User, UserActivityLog, PasswordHistory
 # Register your models here.
+# ✅ Inline: Show password history inside User detail
+class PasswordHistoryInline(admin.TabularInline):
+    model = PasswordHistory
+    extra = 0
+    readonly_fields = ("password_hash", "timestamp")
+    can_delete = False
+    ordering = ("-timestamp",)
+    verbose_name_plural = "Password Change History"\
+    
 admin.site.register(User)
 admin.site.register(UserActivityLog)
 admin.site.register(PasswordHistory)
