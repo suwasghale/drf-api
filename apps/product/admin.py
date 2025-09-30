a4a3dae8f8417e91e3b966c6f32439c8d22a1a9b
SuwasG <suwasghale693@gmail.com>
2025-09-30 18:20:37 +0545

    product specification inline added in admin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Category, Product
+from django.utils.html import format_html
+from apps.product.models import Category, Product, ProductSpecification
+
+# -------------------------------
+# Inline for ProductSpecification
+# -------------------------------
+class ProductSpecificationInline(admin.TabularInline):
+    """
+    Allows adding/editing Product Specifications directly inside Product admin.
+    """
+    model = ProductSpecification
+    extra = 1
+    fields = ("key", "value")
+    show_change_link = True
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
