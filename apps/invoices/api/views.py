02e962a1f1c6837b3f662d60d38a14077bc21689
SuwasG <suwasghale693@gmail.com>
2025-10-10 23:06:24 +0545

    is owner or admin permission added for invoices views

---
+++ apps/invoices/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+from django.shortcuts import get_object_or_404
+from django.http import FileResponse, HttpResponse
+
+from apps.invoices.models import Invoice
+from apps.invoices.api.serializers import InvoiceSerializer, InvoiceCreateSerializer
+from apps.invoices.api.services import create_invoice_for_order
+
+class IsOwnerOrAdmin:
+    """Simple class-based permission used inline (or implement as DRF Permission)."""
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj.order.user == request.user
