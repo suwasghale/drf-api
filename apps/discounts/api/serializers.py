adc280382a476e3ba91b6ae259be4c21dd0bc1ec
SuwasG <suwasghale693@gmail.com>
2025-10-13 23:22:13 +0545

    discount serializer added

---
+++ apps/discounts/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from apps.discounts.models import Discount
+from apps.discounts.models_user_usage import DiscountRedemption
+from django.utils import timezone
+from decimal import Decimal
+
+class DiscountSerializer(serializers.ModelSerializer):
+    remaining_uses = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Discount
+        fields = [
+            "id", "code", "description", "discount_type", "amount",
+            "min_order_value", "usage_limit", "used_count", "per_user_limit",
+            "is_active", "valid_from", "valid_until",
+            "remaining_uses", "created_at", "updated_at"
+        ]
+        read_only_fields = ["used_count", "created_at", "updated_at"]
+
