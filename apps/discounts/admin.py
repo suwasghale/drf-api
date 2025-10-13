d80016f9f245edb5f460a0465c2cb8f2e8658411
SuwasG <suwasghale693@gmail.com>
2025-10-13 23:25:40 +0545

    discount redemption admin

--- apps/discounts/admin.py
+++ apps/discounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from apps.discounts.models import Discount
+from apps.discounts.models_user_usage import DiscountRedemption
 
 # Register your models here.
 
@@ -10,3 +11,8 @@ class DiscountAdmin(admin.ModelAdmin):
     list_filter = ("discount_type", "is_active")
     readonly_fields = ("used_count", "created_at", "updated_at")
 
+@admin.register(DiscountRedemption)
+class DiscountRedemptionAdmin(admin.ModelAdmin):
+    list_display = ("discount", "user", "order", "amount", "created_at")
+    search_fields = ("discount__code", "user__username", "order__id")
+    list_filter = ("discount",)
