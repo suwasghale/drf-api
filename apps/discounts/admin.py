2f5e3f57e9e45e0399392f63bfad931d98c69feb
SuwasG <suwasghale693@gmail.com>
2025-10-13 23:25:24 +0545

    discount admin

--- apps/discounts/admin.py
+++ apps/discounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.discounts.models import Discount
 
 # Register your models here.
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ("code", "discount_type", "amount", "is_active", "used_count", "usage_limit", "valid_from", "valid_until")
+    search_fields = ("code", "description")
+    list_filter = ("discount_type", "is_active")
+    readonly_fields = ("used_count", "created_at", "updated_at")
+
