9c49a0856c9c59b83ed6a5fad6d960e2453f2e64
SuwasG <suwasghale693@gmail.com>
2025-11-02 20:24:56 +0545

    product apps.py

--- apps/product/apps.py
+++ apps/product/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.product'
     label = 'product'
+    verbose_name = _("Product Management")
 
     def ready(self):
         import apps.product.signals  # Ensure signals are imported when the app is ready
