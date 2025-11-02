77b76dc051e4eacaf39a68952e98b3c644e007dc
SuwasG <suwasghale693@gmail.com>
2025-11-02 20:22:28 +0545

    notifications apps.py

--- apps/notifications/apps.py
+++ apps/notifications/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 class NotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.notifications"
+    verbose_name = _("Notifications & Alerts")
 
     def ready(self):
         import apps.notifications.signals # noqa
