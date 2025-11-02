de60b26504ad438b95909495d3a576c59c4f00c5
SuwasG <suwasghale693@gmail.com>
2025-11-02 20:24:23 +0545

    payments apps.py

--- apps/payments/apps.py
+++ apps/payments/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.payments'
+    verbose_name = _("Payments & Transactions")
 
     def ready(self):
         import apps.payments.signals  # noqa: F401
