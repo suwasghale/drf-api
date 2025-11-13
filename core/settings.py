54ef3428b1cd141b89b29f0cae3a58727e235b2a
SuwasG <suwasghale693@gmail.com>
2025-11-13 19:42:27 +0545

    cloudinary secrets added on env and settings

--- core/settings.py
+++ core/settings.py
@@ -246,6 +246,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
