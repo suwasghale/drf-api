4e6b3ccfb55a3abefc3aa97107993ae90235337e
SuwasG <suwasghale693@gmail.com>
2025-11-13 19:51:53 +0545

    cloudinary settings updated

--- core/settings.py
+++ core/settings.py
@@ -246,14 +246,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+# Cloudinary settings
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': env('CLOUDINARY_API_KEY'),
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
 
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
