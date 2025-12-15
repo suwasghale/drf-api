36bb4ad3da95def607640216a2a6780aac2c893a
SuwasG <suwasghale693@gmail.com>
2025-12-15 12:00:00 +0545

    Fix: missed contribution with urls for users updated

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,9 +1,24 @@
+# apps/users/api/urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.users.api.views import UserViewSet
+
+from apps.users.api.viewsets import (
+    AuthViewSet,
+    PasswordViewSet,
+    ProfileViewSet,
+    AdminUserViewSet,
+    SecurityViewSet,
+)
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+router.register(r"auth", AuthViewSet, basename="auth")
+router.register(r"password", PasswordViewSet, basename="password")
+router.register(r"profile", ProfileViewSet, basename="profile")
+router.register(r"admin/users", AdminUserViewSet, basename="admin-users")
+router.register(r"security", SecurityViewSet, basename="security")
+
+app_name = "users"
 
 urlpatterns = [
     path("", include(router.urls)),
