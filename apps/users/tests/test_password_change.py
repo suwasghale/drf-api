b3d389b1c94514f8744af4e0e8d929182e9001cf
SuwasG <suwasghale693@gmail.com>
2025-12-19 17:33:56 +0545

    test wrong old password fails

--- apps/users/tests/test_password_change.py
+++ apps/users/tests/test_password_change.py
@@ -26,3 +26,11 @@ class ChangePasswordAPITestCase(APITestCase):
     def test_authentication_required(self):
         response = self.client.post(self.url, {})
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_wrong_old_password_fails(self):
+        self.authenticate()
+        response = self.client.post(self.url, {
+            "old_password": "WrongPass",
+            "new_password": "NewStrongPass123!"
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
